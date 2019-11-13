c2cdb1fed395ad0dbb90b6429caceb5d83603cc9
Sebastian Ruchlewicz <s.ruchlewicz@wearevirtua.com>
2019-11-13 10:53:20 +0100

    init commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Virtua_SearchFix',
+    __DIR__ . '/src/SearchFix'
+);
